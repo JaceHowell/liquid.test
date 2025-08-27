dfd3dc755128570fe30cf115e6b1d6245ded4159
Jace Howell <jace.howell@Jaces-MacBook-Pro.local>
2025-08-27 12:29:06 -0600

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./layout/**/*.liquid", "./sections/**/*.liquid", "./snippets/**/*.liquid", "./templates/**/*.liquid"],
+
+};
